b873454241d15dd6086ecfa55b747c03ab659753
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2016-03-18 21:36:03 +0100

    Reworked template resolving, currently bugs for root URL

--- gloo/find-template.js
+++ gloo/find-template.js
@@ -1,27 +1,22 @@
 const path = require("path");
 const fs = require("fs");
 const debug = require("debug")("gloo:find-template");
-const config = require("./config");
+const config = require("./../config");
 
 // Finds the template file that fits the request.
 //We should at least always deliver the toplevel default file.
-exports.find = function(req, subdomain) {
-    var site = subdomain + req.path;
-    var pathToLookIn = path.resolve("./templates/" + site)
-    if (site.substr(site.length-1) == "/") {
-        pathToLookIn += path.sep + config.defaultTemplate;
-    }
-    debug("PATHL: " + pathToLookIn);
-    debug("SITE: " + site);
+exports.find = function(req) {
+    var pathToLookIn = path.resolve("./" + config.templateDir + "/" + req.path);
+    debug("PATH: " + pathToLookIn);
 
     var result;
 
-    var explicitTemplatePath = templatePath(pathToLookIn, subdomain);
+    var explicitTemplatePath = resolveTemplate(pathToLookIn);
     if (explicitTemplatePath) {
         result = explicitTemplatePath;
     } else {
         debug("No explicit template found, looking for a default.");
-        result = defaultTemplateOf(pathToLookIn, subdomain);
+        result = defaultTemplateOf(pathToLookIn);
     }
 
     if (result) {
@@ -36,16 +31,16 @@ exports.find = function(req, subdomain) {
 // First looks for a default file (name specified in config.defualtTemplate) in
 // the same directory as given file, then in the directory above, then above
 // that, etc.
-// The last one it will check is ./config.topTemplateDirectory/config.defaultFile.
+// The last one it will check is ./config.templateDirectory/config.defaultFile.
 // That one should always exist, and if some idiot deletes it, bad things might
 // happen.
-function defaultTemplateOf(pathToLookIn, subdomain) {
-    const topDir = path.resolve("./" + config.topTemplateDir + "/" + subdomain + "/");
+function defaultTemplateOf(pathToLookIn) {
+    const topDir = path.resolve("./" + config.templateDir);
     // We shall only look in templateDir, else we'll go into a loop.
     if (isPathInsideDir(pathToLookIn, topDir)) {
         pathToLookIn = path.resolve(path.dirname(pathToLookIn), config.defaultTemplate);
         var pathFound;
-        while (!(pathFound = templatePath(pathToLookIn, subdomain))) {
+        while (!(pathFound = resolveTemplate(pathToLookIn))) {
             var directory = path.dirname(pathToLookIn);
             if (directory == topDir) {
                 debug("BAD!", "Toplevel default template", pathToLookIn, "not found.",
@@ -53,6 +48,7 @@ function defaultTemplateOf(pathToLookIn, subdomain) {
                 return undefined;
             }
             pathToLookIn = path.resolve(directory, "../" + config.defaultTemplate);
+            debug("Trying " + pathToLookIn);
         }
         return pathFound;
     } else {
@@ -60,41 +56,14 @@ function defaultTemplateOf(pathToLookIn, subdomain) {
     }
 }
 
-// Takes a full path to a file too look for, without the extension.
-// Returns undefined if not found with any supported extension.
-function templatePath(fullPath, subdomain) {
-    var templatePathKnownExt = templatePathGuessExt(fullPath, subdomain);
-    if (templatePathKnownExt) {
-        return templatePathKnownExt;
-    } else {
-        return templatePathTryAll(fullPath);
+function resolveTemplate(fullPath) {
+    var fullPathWithExt = fullPath + "." + config.extension;
+    try {
+        fs.accessSync(fullPathWithExt);
+        return fullPathWithExt;
+    } catch (e) {
+        debug("Could not find file " + fullPathWithExt);
     }
-}
-
-function templatePathGuessExt(fullPath, subdomain) {
-    var guessExt = config.knownExtensions[subdomain];
-    var filePath = fullPath + "." + guessExt;
-    if (fileExists(filePath)) {
-        debug("Guessed extension right");
-        return filePath;
-    } else {
-        return undefined;
-    }
-}
-
-function templatePathTryAll(fullPath) {
-    for (var engineName in config.supportedEngines) {
-        var engineDesc = config.supportedEngines[engineName];
-        try {
-            var fullPathWithExt = fullPath + "." + engineDesc.extension;
-            debug("trying" + fullPathWithExt + engineDesc)
-            fs.accessSync(fullPathWithExt);
-            return fullPathWithExt;
-        } catch (e) {
-            continue;
-        }
-    }
-    debug("Could not find file" + fullPath);
     return undefined;
 }
 
@@ -125,19 +94,19 @@ exports.test = function(assert) {
         "Nothing should be returned if we're not in template dir.");
     assert(exports.find({path: "/namnder"}, "www"),
         "This nonexistent template should fallback to default template");
-    assert.equals(path.resolve(config.topTemplateDir, "test", config.defaultTemplate + ".jade"), exports.find({path: "/"}, "test"),
+    assert.equals(path.resolve(config.templateDir, "test", config.defaultTemplate + ".jade"), exports.find({path: "/"}, "test"),
         "Should use default template in subdomain when asked for topdir");
-    assert.equals(path.resolve(config.topTemplateDir, "test/foo", config.defaultTemplate + ".jade"), exports.find({path: "/foo/"}, "test"),
+    assert.equals(path.resolve(config.templateDir, "test/foo", config.defaultTemplate + ".jade"), exports.find({path: "/foo/"}, "test"),
         "Should use default template in foo folder.");
-    assert.equals(path.resolve(config.topTemplateDir, "test", config.defaultTemplate + ".jade"), exports.find({path: "/bar"}, "test"),
+    assert.equals(path.resolve(config.templateDir, "test", config.defaultTemplate + ".jade"), exports.find({path: "/bar"}, "test"),
         "No default in that folder, so it should use toplevel default template");
-    assert.equals(path.resolve(config.topTemplateDir, "test/foo", config.defaultTemplate + ".jade"), exports.find({path: "/foo/baz"}, "test"),
+    assert.equals(path.resolve(config.templateDir, "test/foo", config.defaultTemplate + ".jade"), exports.find({path: "/foo/baz"}, "test"),
         "No default in that folder, so it should climb up a level");
-    assert.equals(path.resolve(config.topTemplateDir, "test/foo", config.defaultTemplate + ".jade"), exports.find({path: "/foo/baz/bad/bac/baf/burbur"}, "test"),
+    assert.equals(path.resolve(config.templateDir, "test/foo", config.defaultTemplate + ".jade"), exports.find({path: "/foo/baz/bad/bac/baf/burbur"}, "test"),
         "Should climb up many levels");
-    assert.equals(path.resolve(config.topTemplateDir, "test/custom.jade"), exports.find({path: "/custom"}, "test"),
+    assert.equals(path.resolve(config.templateDir, "test/custom.jade"), exports.find({path: "/custom"}, "test"),
         "Should use custom template since it exists");
-    assert.equals(path.resolve(config.topTemplateDir, "test/", config.defaultTemplate + ".jade"), exports.find({path: "/custom/"}, "test"),
+    assert.equals(path.resolve(config.templateDir, "test/", config.defaultTemplate + ".jade"), exports.find({path: "/custom/"}, "test"),
         "Should not confuse directory custom with file custom.");
 
 }
