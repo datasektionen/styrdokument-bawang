e3ae5ca8c241ee3a7c1d565683b630d8f7adb1f9
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2016-04-28 21:54:35 +0200

    Styrdok config

---
+++ config.js
@@ -0,0 +1,23 @@
+/**
+ * Gloo configuration
+ */
+module.exports = {
+
+    // External port where Gloo should listen for connections
+    // When deployed, this is usually process.env.PORT
+    port: process.env.PORT || 5000,
+
+    // Hostname of the Taitan instance, and whether to communicate over HTTPS with it
+    taitanHost: "styrdok-taitan.froyo.datasektionen.se",
+    https: true, // Taitan seems to only accept https atm.
+
+    // The name of the template engine as it's called by consolidate.js, may differ from extension
+    // Default template file to look for in subdirectories
+    engine: "nunjucks",
+    extension: "nunj",
+    defaultTemplate: "_default",
+
+    // Directories this gloo instance uses to source its content
+    templateDir: "templates",
+    staticDir: "static"
+};
