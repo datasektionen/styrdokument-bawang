b0cdba638b8d1a7bf004e81e760f4bca07d98b8e
Mathias Magnusson <mathias@magnusson.space>
2024-05-26 15:30:31 +0200

    Change default taitan url

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
     port: process.env.PORT || 5000,
 
     // Hostname of the Taitan instance, and whether to communicate over HTTPS with it
-    taitanUrl: process.env.TAITAN_URL || "https://styrdokument-taitan.mjukglass.datasektionen.se",
+    taitanUrl: process.env.TAITAN_URL || "https://taitan-styrdokument.datasektionen.se",
 
     // The name of the template engine as it's called by consolidate.js, may differ from extension
     // Default template file to look for in subdirectories
